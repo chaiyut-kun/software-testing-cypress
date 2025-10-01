75f6afc0d0880b67c4a87acb75f700a500d98fb7
chaiyut-kun <chwork28@hotmail.com>
2025-10-01 23:25:35 +0700

    request api with POST method using register sucess data (JSON)

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,22 +2,24 @@ describe("Login test", () => {
   it.only("should login Fail", () => {
     const interceptedData = [];
     let loginRequest;
-    //   ดึงข้อมูลจากไฟล์ intercepted_post_data.json
-    cy.fixture("intercepted_post_data.json").then((apiData) => {
-      // ใช้ข้อมูลตัวแรกจาก array
-      loginRequest = apiData[0];
-      // คาดหวังว่า status code จะเป็น 200
-      expect(loginRequest.statusCode).to.eq(200);
-      
-      cy.request({
-        method: loginRequest.method,
-        url: loginRequest.url,
-        body: loginRequest.requestBody,
-        failOnStatusCode: false,
-      }).then((res) => {
-        cy.log("Response Body:", JSON.stringify(res.body, null, 2));
-      });
-    });
 
+    //   ดึงข้อมูลจากไฟล์ intercepted_post_data.json
+    cy.fixture("intercepted_post-register-success.json").then((apiData) => {
+      for (let i = 0; i < apiData.length; i++) {
+        // ใช้ข้อมูลตัวแรกจาก array
+        loginRequest = apiData[i];
+        // คาดหวังว่า status code จะเป็น 200
+        expect(loginRequest.statusCode).to.eq(201);
+
+        cy.request({
+          method: loginRequest.method,
+          url: loginRequest.url,
+          body: loginRequest.requestBody,
+          failOnStatusCode: false,
+        }).then((res) => {
+          cy.log("Response Body:", JSON.stringify(res.body, null, 2));
+        });
+      }
+    });
   });
 });
